40745f8059247527c5bb1415b7a622c445b19829
shoji-ishida <shoji.ishida@gmail.com>
2014-08-11 11:50:36 +0900

    Initial Commit

---
+++ HomeKitSample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HomeKitSample
+//
+//  Created by 石田 勝嗣 on 2014/08/11.
+//  Copyright (c) 2014年 石田 勝嗣. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HomeKitSample/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  HomeKitSample
+//
+//  Created by 石田 勝嗣 on 2014/08/11.
+//  Copyright (c) 2014年 石田 勝嗣. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+            
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
